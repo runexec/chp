f4b349ae987ef15dfdd0750491febb5a56fcf7dd
Ryan Kelker <theinternet@yandex.com>
2013-05-12 10:47:07 +0900

    Upgraded lein-cljsbuild

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
                  [garden "0.1.0-beta2"]
                  [korma "0.3.0-RC5"]]
   :plugins [[lein-ring "0.8.3"]
-            [lein-cljsbuild "0.3.0"]]
+            [lein-cljsbuild "0.3.1"]]
   :ring {:handler chp.handler/app}
   :cljsbuild {:builds [{:source-paths ["resources/cljs"]
                         :compiler {:output-to "resources/public/js/main.js"
