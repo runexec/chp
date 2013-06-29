689a0ebbb72e2bd177cccf22c61b0dbeb269c5db
Ryan Kelker <theinternet@yandex.com>
2013-06-29 23:52:59 +0900

    sha-512 is the default password strength + salt fn

--- src/chp/password.clj
+++ src/chp/password.clj
@@ -3,9 +3,21 @@
 (defn password [salt s]
   (let [password (str salt s)]
     (->> (doto (java.security.MessageDigest/getInstance 
-                "SHA-1")
+                "SHA-512")
            .reset
            (.update (.getBytes password)))
          .digest
          (map #(format "%02x" %))
          (apply str))))
+
+(defn salt
+  ([] (salt
+       (reduce #(password %2 %1)
+               (take 10
+                     (iterate rand-int 100)))))
+  ([& [base]]
+     (if base
+       (password base base)
+       base)))
+               
+                    
