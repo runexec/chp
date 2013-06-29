da6b8a2eee7dc0f9f3f796eec4563d80132488f4
Ryan Kelker <theinternet@yandex.com>
2013-06-30 00:27:46 +0900

    added documentation to admin migration

--- resources/migrations/01-add-admin.clj
+++ resources/migrations/01-add-admin.clj
@@ -1,4 +1,14 @@
 (let [table (kc/create-entity "user")]
+
+  (comment Assuming resources/schema/user.clj
+           (table :user
+                  (integer :id :primary-key :auto-inc)
+                  (varchar :name 20)
+                  (varchar :password 128)
+                  (varchar :salt 128)
+                  (boolean :admin)
+                  (unique [:name])))
+
   (defmigration create-chp-admin
     (up []
         (let [{:keys [salt
